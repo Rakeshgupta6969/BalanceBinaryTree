103608827b7fdf57409cb00918ba6b215ff520b3
Rakesh Gupta <rg688407@gmail.com>
2025-08-12 22:08:38 +0530

    optimal solution

---
+++ sol.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+
+ private:
+
+ pair<bool,int> solve(TreeNode* root){
+
+   // base case of the recursion.
+
+   if(root == NULL){
+    pair<bool,int> p = make_pair(true,0);
+    return p;
+   }
+
+
+   pair<bool,int> left = solve(root -> left);
+   pair<bool,int> right = solve(root -> right);
+
+
+   pair<bool,int> temp;
+
+
+   temp.first =  left.first && right.first && (fabs(left.second - right.second) <= 1);
+
+   temp.second = max(left.second,right.second) +1;
+
+
+   return temp;
+
+
+
+
+
+
+ }
+
+
+
+
+public:
+    bool isBalanced(TreeNode* root) {
+
+    // using the concept of the recursion.
+
+
+    if(root == NULL) return true;
+
+    // using the concept of the pair data structure.
+
+    pair<bool,int> ans = solve(root);
+
+
+    return ans.first;
+
+
+
+
+    }
+};
